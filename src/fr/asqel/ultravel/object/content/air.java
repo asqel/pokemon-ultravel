f644b20ca09fe6a63bc674a812a95463e52fc6fe
asqel <91128487+asqel@users.noreply.github.com>
2024-04-14 15:05:38 +0200

    update

---
+++ src/fr/asqel/ultravel/object/content/air.java
@@ -0,0 +1,15 @@
+package fr.asqel.ultravel.object.content;
+
+import java.awt.image.BufferedImage;
+import java.util.Optional;
+
+import fr.asqel.ultravel.object.object;
+
+public class air extends object{
+
+	public air(int x, int y) {
+		super(x, y, id, texture);
+		//TODO Auto-generated constructor stub
+	}
+	
+}
